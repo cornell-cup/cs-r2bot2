395eb0dfb17140fbd2fca42873e4e07480e8abd9
Jimmy Chen <jzc8@cornell.edu>
2016-10-23 12:49:56 -0400

    Add ManualControls

---
+++ R2ManualControls/ConsoleApplication1/XBoxController.h
@@ -0,0 +1,48 @@
+#ifndef XBOXCONTROLLER_H
+#define XBOXCONTROLLER_H
+
+#include <Windows.h>
+#include <Xinput.h>
+#include <iostream>
+
+class XBoxController
+{
+public:	
+
+	float leftStickX;
+	float leftStickY;
+	float rightStickX;
+	float rightStickY;
+	float leftVoltage;
+	float rightVoltage;
+
+	// Constructors and Destructors
+	XBoxController();
+	XBoxController(float, float);
+	~XBoxController();
+
+	//Accessor Functions
+	float getDeadzoneX() const;
+	float getDeadzoneY() const;
+	XINPUT_GAMEPAD *getState();
+	bool CheckConnection();
+	bool Refresh();
+
+	//Mutator Functions
+	void setDeadzoneX(float);
+	void setDeadzoneY(float);
+	void setMaxVoltage(float);
+	void calcVoltage(float, float);
+
+private:
+	int controllerId;
+	XINPUT_STATE state;
+	float deadzoneX;
+	float deadzoneY;
+	float maxVoltage;
+
+
+
+};
+
+#endif
