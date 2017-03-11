7319a6056ea96c32edd6ada424e742a3f7247550
Scott Wu <scott.wu.hjx@gmail.com>
2017-03-11 12:27:47 -0500

    Fix gitignore

---
+++ R2Bot/lib/README.md
@@ -0,0 +1,5 @@
+# R2Bot External Libraries
+
+## Instructions
+
+Run each of the scripts, or read the comments within them to download, compile them yourself.
---
+++ R2Bot/lib/crow.sh
@@ -0,0 +1,9 @@
+# crow C++ Web Framework
+# https://github.com/ipkn/crow
+
+# crow is a header only C++ framework
+# 1. Clone crow from https://github.com/ipkn/crow
+# 2. Add `crow/amalgamate` to the include path
+# 3. Include `crow_all.h`
+
+git clone https://github.com/ipkn/crow
