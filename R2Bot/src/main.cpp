c361abec3d6b04b6ef06f9e83356f82474401c99
Eleng555 <ice_el@sbcglobal.net>
2017-03-14 16:15:17 -0400

    merge framework files

--- R2Bot/src/main.cpp
+++ R2Bot/src/main.cpp
@@ -3,7 +3,6 @@
 #include "Controller.h"
 #include "JobHandler.h"
 #include "JobQueue.h"
-#include "R2Server.h"
 #include "Sensor.h"
 #include <vector>
 #include <iostream>
@@ -121,85 +120,5 @@ void processManualData() {
 				}
 			}
 		}
-	}
-}
-
-int main(int argc, char *argv[]) {
-	std::string host = "";
-	int port = -1;
-
-	/** Handle arguments */
-	for (int i = 0; i < argc; i++) {
-		std::string arg = std::string(argv[i]);
-		if (arg == "--jobs-file") {
-			// Set initial jobs file for job_queue initialization
-			if (++i < argc) {
-				jobQueue.addJobsFile(argv[i]);
-			}
-			else {
-				std::cout << "jobs-file flag requires an argument";
-			}
-		}
-		else if (arg == "--host") {
-			 //Set network host for job_queue initialization
-			if (++i < argc)
-				host = arg;
-			else
-				std::cout << "host flag requires an argument";
-		}
-		else if (arg == "--port") {
-			// Set network port for job_queue initialization
-			if (++i < argc)
-				port = stoi(arg);
-			else
-				std::cout << "port flag requires an argument";
-		}
-		else {
-			// Unrecognized flag
-			std::cout << "Unrecognized flag " << arg;
-		}
-	}
-
-	if (host != "" && port >= 0)
-		jobQueue.setNetworkInterface(host, port);
-
-	/** Initialization */
-	initializeSensors();
-	initializeManualInputsHandler("192.168.4.170", 9020);
-	initializeControllers();
-	initializeJobHandlers();
-	initializeJobQueue();
-
-	/** Create web server for displying image stream */
-	char* server_args[] = {"R2Server", "--docroot", ".",
-		"--http-address", "0.0.0.0", "--http-port", "8080"};
-	KinectSensor *kSensor = static_cast<KinectSensor *>(sensors["KinectSensor"]);
-	std::thread serverThread(R2Server::run,
-		kSensor->imageWidth,
-		kSensor->imageHeight,
-		7, server_args);
-	std::cout << "\nStarted Server thread\n";
-
-	/** Create manual input thread */
-	std::thread manualInputThread(processManualData);
-	std::cout << "\nStarted Manual Input Thread\n";
-
-	/** Main execution loop */
-	while (1) {
-		std::string job = jobQueue.getJob();
-		if (job != "") {
-			JobHandler *handler = getHandlerByJob(job);
-			handler->execute(job);
-		}
-		getSensorData();
-		// Update server image. TODO: maybe move this?
-		if (R2Server::k != nullptr) {
-			kSensor->imageMutex->lock();
-			R2Server::k->setImage(kSensor->jpgImage,
-				kSensor->jpgSize);
-			kSensor->imageMutex->unlock();
-		}
-		break;
-		 //TODO: Complete this function
 	}
 }
--- R2bot/src/R2Server.cpp
+++ R2bot/src/R2Server.cpp
@@ -1,31 +1,151 @@
-#include "R2Server.h"
+// CrowTestSample.cpp : Defines the entry point for the console application.
+//
+#define _CRT_SECURE_NO_WARNINGS
+#define _SCL_SECURE_NO_WARNINGS
 
-#define BYTES_PER_PIXEL 4
+#include "crow_all.h"
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <urlmon.h>
+#include <unordered_set>
+#include <mutex>
 
-// Initialize static variables
-int R2Server::_imageWidth;
-int R2Server::_imageHeight;
-KinectImageResource *R2Server::k = nullptr;
+#pragma comment( lib, "urlmon" )
 
-R2Server::R2Server()
+
+std::vector< char > fileContents;
+
+struct Middleware
 {
-	//setTitle("R2Server");
-	
-	// Add Widgets to the webpage
+	std::string message;
 
-	// Timer event to update image on page
+	Middleware()
+	{
+		message = " ";
+	}
+
+	void setMessage(std::string newMsg)
+	{
+		message = newMsg;
+	}
+
+	struct context
+	{
+	};
+
+	void before_handle(crow::request& /*req*/, crow::response& /*res*/, context& /*ctx*/)
+	{
+		CROW_LOG_DEBUG << " - MESSAGE: " << message;
+	}
+
+	void after_handle(crow::request& /*req*/, crow::response& /*res*/, context& /*ctx*/)
+	{
+		// no-op
+	}
+};
+
+std::string readIn(std::string fileName) {
+	{
+		std::ifstream file(fileName, std::ios::in | std::ios::binary | std::ios::ate);
+		if (!file.is_open())
+			throw std::runtime_error("couldn't open");
+
+		fileContents.resize(file.tellg());
+
+		file.seekg(0, std::ios::beg);
+		if (!file.read(&fileContents[0], fileContents.size()))
+			throw std::runtime_error("failed to read");
+	}
+	return std::string(fileContents.data(), fileContents.size());
 }
 
-void R2Server::refreshKinectImage()
+std::wstring MimeTypeFromString(const std::wstring& str) {
+
+	LPWSTR pwzMimeOut = NULL;
+	HRESULT hr = FindMimeFromData(NULL, str.c_str(), NULL, 0,
+		NULL, FMFD_URLASFILENAME, &pwzMimeOut, 0x0);
+	if (SUCCEEDED(hr)) {
+		std::wstring strResult(pwzMimeOut);
+		// Despite the documentation stating to call operator delete, the
+		// returned string must be cleaned up using CoTaskMemFree
+		CoTaskMemFree(pwzMimeOut);
+		return strResult;
+	}
+
+	return L"";
+}
+
+// Convert an UTF8 string to a wide Unicode String
+std::wstring utf8_decode(const std::string &str)
 {
-	//doJavaScript("document.getElementById('img').src = 'kinectImage?t=' + new Date().getTime()");
+	if (str.empty()) return std::wstring();
+	int size_needed = MultiByteToWideChar(CP_UTF8, 0, &str[0], (int)str.size(), NULL, 0);
+	std::wstring wstrTo(size_needed, 0);
+	MultiByteToWideChar(CP_UTF8, 0, &str[0], (int)str.size(), &wstrTo[0], size_needed);
+	return wstrTo;
+}
+
+// Convert a wide Unicode string to an UTF8 string
+std::string utf8_encode(const std::wstring &wstr)
+{
+	if (wstr.empty()) return std::string();
+	int size_needed = WideCharToMultiByte(CP_UTF8, 0, &wstr[0], (int)wstr.size(), NULL, 0, NULL, NULL);
+	std::string strTo(size_needed, 0);
+	WideCharToMultiByte(CP_UTF8, 0, &wstr[0], (int)wstr.size(), &strTo[0], size_needed, NULL, NULL);
+	return strTo;
+}
+
+int main() {
+
+	crow::SimpleApp app;
+	//	app.get_middleware<Middleware>().setMessage("hello");
+
+	std::mutex mtx;;
+	std::unordered_set<crow::websocket::connection*> users;
+
+	//crow::mustache::set_base(".");
+
+	CROW_ROUTE(app, "/ws")
+		.websocket()
+		.onopen([&](crow::websocket::connection& conn) {
+		CROW_LOG_INFO << "new websocket connection";
+		std::lock_guard<std::mutex> _(mtx);
+		users.insert(&conn);
+	})
+		.onclose([&](crow::websocket::connection& conn, const std::string& reason) {
+		CROW_LOG_INFO << "websocket connection closed: " << reason;
+		std::lock_guard<std::mutex> _(mtx);
+		users.erase(&conn);
+	})
+		.onmessage([&](crow::websocket::connection& /*conn*/, const std::string& data, bool is_binary) {
+		std::lock_guard<std::mutex> _(mtx);
+		for (auto u : users)
+			if (is_binary) {
+				u->send_binary("bee.png");
+
+				std::cout << data;
+				std::cout << "hello";
+			}
+			else {
+				u->send_binary(readIn("bee.png"));
+				std::cout << "bee.png" << std::endl;
+				//std::cout << data;
+			}
+	});
+
+	CROW_ROUTE(app, "/<string>") ([](const crow::request& req, crow::response& res, std::string str) {
+		int index = str.rfind('.');
+		std::wstring place = utf8_decode(str.substr(index));
+		std::string str1(utf8_encode(MimeTypeFromString(place)));
+		res.add_header("Content-Type", str1);
+		res.write(readIn(str));
+		res.end();
+	});
+
+	app.port(18080).multithreaded().run();
 }
 
 
-int R2Server::run(int imageWidth, int imageHeight, int argc, char **argv)
-{
-	R2Server::_imageWidth = imageWidth;
-	R2Server::_imageHeight = imageHeight;
-	//return WRun(argc, argv, &createApplication);
-	return 1;
-}
+
+
