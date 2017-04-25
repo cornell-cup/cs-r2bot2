39024ab8267bc66679e1c198addfbb3d7cade8b0
Eleng555 <ice_el@sbcglobal.net>
2017-04-25 17:24:06 -0400

    add drawers

--- R2Bot/src/DrawerSensor.cpp
+++ R2Bot/src/DrawerSensor.cpp
@@ -3,24 +3,7 @@
 
 DrawerSensor::DrawerSensor(string port, int baudrate) : Sensor("Drawer Sensor"), conn(std::make_shared<SerialPort>(port, baudrate)),
 dataMutex(), dataReceived(), dataToForward() {
-	std::lock_guard<std::mutex> lock(dataMutex);
-	if (conn->isConnected()) {
-		char data[256];
-		int bytesRead = conn->read(data, 256);
-
-		// Decode the incoming data
-		R2Protocol::Packet params;
-		std::vector<unsigned char> input(data, data + bytesRead);
-		R2Protocol::decode(input, params); 
-		if (params.destination == DEVICE_NAME) {
-			// Data is sent here
-			dataReceived[params.source] = params.data;
-		}
-		else {
-			// Data should be forwarded
-			dataToForward[params.destination] = params;
-		}
-	}
+	
 }
 
 DrawerSensor::~DrawerSensor() {
@@ -31,5 +14,5 @@ bool DrawerSensor::ping() {
 }
 
 void DrawerSensor::fillData(smap<void*>& sensorData) {
-	// TODO 
+	
 }
