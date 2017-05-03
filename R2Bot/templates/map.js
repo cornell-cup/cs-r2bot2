5f0b6d89f0ebb499a21fb734dd91123cf91f3dfc
Eleng555 <ice_el@sbcglobal.net>
2017-05-03 18:06:28 -0400

    clean map.js

--- R2Bot/templates/map.js
+++ R2Bot/templates/map.js
@@ -26,53 +26,39 @@ function drawMap(data) {
 
     var dist = arrayBufferToString(data);
 
-    console.log(dist);
-
     //splitting up array of sensor data and creating dictionary of sensor:sensorData
     var n = dist.split("\n");
-    console.log(n);
 
     var dict = {};
     for (var i = 0; i < n.length; i++) {
         var split = n[i].split(',');
-        //   var subs = split[0].substring(1);
-        // subs = subs.substring(0, subs.length - 1);
         if (parseFloat(split[1]) <= 20.0) {
             dict[split[0]] = parseFloat(split[1]) / 2;
         }
-        console.log(dict);
     }
     //storing values in sensor and distance 
     for (var key in dict) {
         var sensor = key;
-        console.log(sensor);
         var distance = dict[key];
-        console.log(getAngle(sensor));
         angle = parseFloat(getAngle(sensor)) * Math.PI / 180;
-        console.log("angle" + angle);
         findEdge(distance, angle, sensor);
     }
 
     for (var i = 0; i < map.length; i++) {
         for (var j = 0; j < map[i].length; j++) {
             if (map[i][j] === 1) {
-                console.log("this is red");
                 mctx.fillStyle = 'Red';
                 mctx.fillRect(j * 10, i * 10, 10, 10);
-                //  map[i][j + dist] = 2;
             }
             if (map[i][j] == 2) {
-                console.log("this is yellow");
                 mctx.fillStyle = 'Yellow';
                 mctx.fillRect(j * 10, i * 10, 10, 10);
             }
             if (map[i][j] == 3) {
-                console.log("this is cyan");
                 mctx.fillStyle = 'Cyan';
                 mctx.fillRect(j * 10, i * 10, 10, 10);
             }
             if (map[i][j] == 4) {
-                console.log("this is green");
                 mctx.fillStyle = 'Green';
                 mctx.fillRect(j * 10, i * 10, 10, 10);
             }
@@ -87,12 +73,7 @@ function drawMap(data) {
         console.log("ca" + centerAngle);
         for (var i = 0; i < 4; i++) { //going counterclockwise
             var xComponent = Math.floor(distance * Math.cos(centerAngle));
-            console.log(distance * Math.cos(centerAngle));
             var yComponent = Math.floor(distance * Math.sin(centerAngle));
-            console.log("going ccw");
-            // console.log(xComponent);
-            console.log(yComponent);
-
             map[15 - yComponent][15 + xComponent] = getColor(sensor);
             centerAngle += 5 * Math.PI / 180;
         }
@@ -100,9 +81,6 @@ function drawMap(data) {
         for (var i = 0; i < 4; i++) { //going clockwise
             var xComponent = Math.floor(distance * Math.cos(angle));
             var yComponent = Math.floor(distance * Math.sin(angle));
-            console.log("going cw");
-            console.log(xComponent);
-            console.log(yComponent);
             map[15 - yComponent][15 + xComponent] = getColor(sensor);
             angle -= 5 * Math.PI / 180;
         }
