f7756bd335b7ca5e9d79061245cf5d636391bde8
Eleng555 <ice_el@sbcglobal.net>
2017-05-05 22:47:31 -0400

    add sound controller

---
+++ R2Bot/include/Controller/SoundController.h
@@ -0,0 +1,18 @@
+#ifndef _R2BOT_SOUND_CONTROLLER
+#define _R2BOT_SOUND_CONTROLLER
+
+#include "Global.h"
+#include "Controller.h"
+
+class SoundController : public Controller {
+protected:
+
+public:
+	SoundController();
+	virtual ~SoundController();
+
+	virtual bool ping();
+	virtual void sendData(ControllerData& data);
+};
+
+#endif
---
+++ R2Bot/src/SoundController.cpp
@@ -0,0 +1,42 @@
+#include "Controller/SoundController.h"
+#ifdef _WIN32
+#	include <Windows.h>
+#else
+#	include <unistd.h>
+#	include <stdlib.h>
+#endif
+
+#include "R2Protocol.hpp"
+#include <cstring>
+#include <thread>
+
+SoundController::SoundController() : Controller("Sound Controller") {
+	printf("Sound controller connected\n");
+}
+
+SoundController::~SoundController() {
+}
+
+bool SoundController::ping() {
+	return true;
+}
+
+void SoundController::sendData(ControllerData& data) {
+	auto result = data.find("SOUND");
+	if (result != data.end()) {
+		ptr<string> s = std::static_pointer_cast<string>(result->second);
+		string path = "../R2Bot/templates/sounds/" + *s;
+#ifdef _WIN32
+		std::wstring wpath = std::wstring(path.begin(), path.end());
+		PlaySound(wpath.c_str(), NULL, SND_FILENAME | SND_ASYNC);
+#else
+		std::thread sound([data]() {
+			execlp("/usr/bin/aplay", " ", path.c_str(), NULL);		//Execute file: file, arguments (1 or more strings followed by NULL)
+			_exit(0);
+			printf("Playing sound: %s\n", s);
+			}
+		);
+		sound.detach();
+#endif	
+	}
+}
